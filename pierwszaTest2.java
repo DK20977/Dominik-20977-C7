c52ad0223aafeb38877f621b9f6e84672b7d4b15
DK-20977-C7 <domino125125@o2.pl>
2023-06-24 21:53:21 +0200

    funkcja na obliczanie delty

---
+++ pierwszaTest2.java
@@ -0,0 +1,9 @@
+public class pierwszaTest2 {
+    public static void main(String[] args) {
+        double a = 2;
+        double b = 5;
+        double c = 3;
+        double delta = Math.pow(b,2)-4*a*c;
+        System.out.println("Delta z równania to: "+delta);
+    }
+}
