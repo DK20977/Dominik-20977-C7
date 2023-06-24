1311162f4cf3b4c331a16b2bf38745dad5e9182f
DK-20977-C7 <domino125125@o2.pl>
2023-06-24 22:18:27 +0200

    obliczanie delty

--- pierwszaTest2.java
+++ pierwszaTest2.java
@@ -3,7 +3,7 @@ public class pierwszaTest2 {
         double a = 2;
         double b = 5;
         double c = 3;
-        double delta =Math.pow(b,2)-4*a*c;
+        double delta=Math.pow(b,2)-4*a*c;
         System.out.println("Delta z równania to: "+delta);
     }
 }
